b33ecd1f90f9423a638ab2f0ceef2bcdf16e29eb
dima3sh <dima.shisha.03@gmail.com>
2022-12-13 18:12:27 +0300

    add external controller

---
+++ ExternalController/src/main/java/org/music/box/external/controller/configuration/music/playlist/dto/UserSongRequestDto.java
@@ -0,0 +1,12 @@
+package org.music.box.external.controller.configuration.music.playlist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSongRequestDto {
+
+    private String songId;
+    private String userId;
+}
