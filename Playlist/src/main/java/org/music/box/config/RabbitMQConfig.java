87500d63ac9538eac2f60daa45122e923aa9943e
dima3sh <dima.shisha.03@gmail.com>
2023-01-13 09:50:03 +0300

    searching songs by substring

--- Playlist/src/main/java/org/music/box/config/RabbitMQConfig.java
+++ Playlist/src/main/java/org/music/box/config/RabbitMQConfig.java
@@ -1,15 +1,10 @@
 package org.music.box.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +12,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
-
-    @Value("${rabbitmq.queue.name}")
-    private String queue;
-
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
-
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
-
-    @Bean
-    public Queue queue(){
-        return new Queue(queue);
-    }
-
-    @Bean
-    public DirectExchange exchange(){
-        return new DirectExchange(exchange);
-    }
 
     @Bean
     public MessageConverter jackson2MessageConverter() {
@@ -49,13 +25,5 @@ public class RabbitMQConfig {
         configurer.configure(factory, connectionFactory);
         factory.setMessageConverter(jackson2MessageConverter());
         return factory;
-    }
-
-    @Bean
-    public Binding binding(){
-        return BindingBuilder
-                .bind(queue())
-                .to(exchange())
-                .with(routingKey);
     }
 }
