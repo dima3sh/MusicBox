d97669638278ced7ab466f911d471b44bb77efaf
dima3sh <dima.shisha.03@gmail.com>
2023-04-10 12:12:17 +0300

    'hello'

--- MusicParser/src/main/java/org/musix/box/parser/repository/SongRepository.java
+++ MusicParser/src/main/java/org/musix/box/parser/repository/SongRepository.java
@@ -1,7 +1,8 @@
 package org.musix.box.parser.repository;
 
 import org.musix.box.parser.documents.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MusicRepository extends MongoRepository<Song, String> {
+public interface MusicRepository extends JpaRepository<Song, String> {
 }
